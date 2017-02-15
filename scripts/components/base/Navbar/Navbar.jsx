cc440bcd05e5ecc8865c2ec6005b81699a052be1
Victor Hiroki <vhiroki@gmail.com>
2017-02-15 12:00:21 -0200

    Home quase pronta!

--- scripts/components/base/Navbar/Navbar.jsx
+++ scripts/components/base/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import logoSrc from './images/logo.png';
 
 export default () => {
     return (
         <nav className="white" role="navigation">
             <div className="nav-wrapper container">
-                <a id="logo-container" href="#" className="brand-logo">Logo</a>
+                <a id="logo-container" style={{height: '100%'}} href="#" className="brand-logo">
+                    <img style={{height: '100%'}} src={logoSrc} />
+                </a>
                 <ul className="right hide-on-med-and-down">
-                    <li><a href="#">Navbar Link</a></li>
+                    <li><a href="#">Já sou Baba</a></li>
                 </ul>
-
                 <ul id="nav-mobile" className="side-nav">
-                    <li><a href="#">Navbar Link</a></li>
+                    <li><a href="#">Já sou Baba</a></li>
                 </ul>
                 <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
             </div>
--- scripts/components/views/Home/Home.jsx
+++ scripts/components/views/Home/Home.jsx
@@ -3,6 +3,7 @@ import background1 from './images/background1.jpg';
 import background2 from './images/background2.jpg';
 import background3 from './images/background3.jpg';
 import $ from 'jquery';
+import {Link} from 'react-router';
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -18,12 +19,12 @@ export default class Home extends React.Component {
                     <div className="section no-pad-bot">
                         <div className="container">
                             <br/><br/>
-                            <h1 className="header center teal-text text-lighten-2">Parallax Template</h1>
+                            <h1 className="header center text-lighten-2">Fique tranquilo!</h1>
                             <div className="row center">
-                                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                                <h5 className="header col s12 light">Nós estaremos ao lado dos seus filhos quando você não puder.</h5>
                             </div>
                             <div className="row center">
-                                <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+                                <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light">Seja uma Babá</a>
                             </div>
                             <br/><br/>
                         </div>
@@ -35,92 +36,31 @@ export default class Home extends React.Component {
                 <div className="container">
                     <div className="section">
                         <div className="row">
-                            <div className="col s12 m4">
-                                <div className="icon-block">
-                                    <h2 className="center brown-text"><i className="material-icons">flash_on</i></h2>
-                                    <h5 className="center">Speeds up development</h5>
-                                    <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
-                                </div>
-                            </div>
-                            <div className="col s12 m4">
-                                <div className="icon-block">
-                                    <h2 className="center brown-text"><i className="material-icons">group</i></h2>
-                                    <h5 className="center">User Experience Focused</h5>
-
-                                    <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-                                </div>
-                            </div>
-                            <div className="col s12 m4">
-                                <div className="icon-block">
-                                    <h2 className="center brown-text"><i className="material-icons">settings</i></h2>
-                                    <h5 className="center">Easy to work with</h5>
-                                    <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="parallax-container valign-wrapper">
-                    <div className="section no-pad-bot">
-                        <div className="container">
-                            <div className="row center">
-                                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="parallax"><img src={background2} alt="Unsplashed background img 2"/></div>
-                </div>
-                <div className="container">
-                    <div className="section">
-                        <div className="row">
                             <div className="col s12 center">
-                                <h3><i className="mdi-content-send brown-text"></i></h3>
-                                <h4>Contact Us</h4>
-                                <p className="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+                                <h5>Para quando eu preciso de uma babá?</h5>
+                                <div className="search-ops-buttons">
+                                    <a className="waves-effect waves-light btn">Para já!</a>
+                                    <a className="waves-effect waves-light btn">Para essa semana</a>
+                                    <a className="waves-effect waves-light btn">Ver Todas</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="parallax-container valign-wrapper">
-                    <div className="section no-pad-bot">
-                        <div className="container">
-                            <div className="row center">
-                                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="parallax"><img src={background3} alt="Unsplashed background img 3"/></div>
-                </div>
-                <footer className="page-footer teal">
+                <footer className="page-footer">
                     <div className="container">
                         <div className="row">
                             <div className="col l6 s12">
-                                <h5 className="white-text">Company Bio</h5>
-                                <p className="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                                <h5 className="white-text">Ali com você, quando e onde precisar.</h5>
+                                <p className="grey-text text-lighten-4">BabaExpress é o nome da maior rede de Babás do mundo. Nós conectamos pessoas atenciosas, com energia e paciência para cuidar de quem você ama.</p>
                             </div>
                             <div className="col l3 s12">
-                                <h5 className="white-text">Settings</h5>
+                                <h5 className="white-text">Navegar</h5>
                                 <ul>
-                                    <li><a className="white-text" href="#!">Link 1</a></li>
-                                    <li><a className="white-text" href="#!">Link 2</a></li>
-                                    <li><a className="white-text" href="#!">Link 3</a></li>
-                                    <li><a className="white-text" href="#!">Link 4</a></li>
+                                    <li><Link className="white-text" to="home">Home</Link></li>
+                                    <li><Link className="white-text" to="list">Busca</Link></li>
                                 </ul>
                             </div>
-                            <div className="col l3 s12">
-                                <h5 className="white-text">Connect</h5>
-                                <ul>
-                                    <li><a className="white-text" href="#!">Link 1</a></li>
-                                    <li><a className="white-text" href="#!">Link 2</a></li>
-                                    <li><a className="white-text" href="#!">Link 3</a></li>
-                                    <li><a className="white-text" href="#!">Link 4</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="footer-copyright">
-                        <div className="container">
-                            Made by <a className="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
                         </div>
                     </div>
                 </footer>
